41cac291593d1fb6f62d93c6f7cd1e219ac18ec2
AndrewBB-BBD <andrewb@bbd.co.za>
2022-02-20 16:02:00 +0200

    Update SCA_DB_script.sql
    feat: added area and location tables

--- SCA_DB_script.sql
+++ SCA_DB_script.sql
@@ -19,3 +19,21 @@ CREATE TABLE dbo.[Race_Lookup] (
   [Race_Description] varchar(150) NOT NULL
  
 )
+
+CREATE TABLE [Area] (
+  [AreaID] [int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
+  [ZIP_Code] [int],
+  [Suburb] [varchar](150),
+  [City] [varchar](150) ,
+  [Province] [varchar](150),
+);
+
+
+CREATE TABLE [Location] (
+  [LocationID] [int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
+  [Area_Code_ID] [int] FOREIGN KEY REFERENCES [Area]([AreaID]) NOT NULL,
+  [Street_Number] [int],
+  [Street_Name] [varchar](250),
+  [Latitude] [decimal](10,6),
+  [Longitude] [decimal](10,6),
+);
