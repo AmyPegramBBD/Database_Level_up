1f88e06fe8db9f330afdc14e4f6f015280c14444
AmyPegramBBD <97732277+AmyPegramBBD@users.noreply.github.com>
2022-02-20 16:04:34 +0200

    Added create statements for assailant and user table

--- SCA_DB_script.sql
+++ SCA_DB_script.sql
@@ -13,9 +13,40 @@ CREATE TABLE dbo.[Gender_Lookup] (
   [Gender_Description] [varchar](150) NOT NULL
  
 );
+GO
 
 CREATE TABLE dbo.[Race_Lookup] (
   [ID] [int] IDENTITY (1,1) NOT NULL PRIMARY KEY,
   [Race_Description] varchar(150) NOT NULL
  
-)
+);
+GO
+
+CREATE TABLE dbo.[User] (
+  [ID] INT IDENTITY (1,1) PRIMARY KEY NOT NULL,
+  [Gender_ID] INT NOT NULL,
+  [Race_ID] INT NOT NULL,
+  [Phone_Number] varchar(15) NULL,
+  [Age] INT NULL,
+  CONSTRAINT [FK_User.Gender_ID]
+    FOREIGN KEY ([Gender_ID])
+      REFERENCES [Gender_Lookup]([ID]),
+  CONSTRAINT [FK_User.Race_ID]
+    FOREIGN KEY ([Race_ID])
+      REFERENCES [Race_Lookup]([ID])
+);
+GO
+
+CREATE TABLE dbo.[Assailant] (
+  [ID] INT IDENTITY (1,1) PRIMARY KEY NOT NULL,
+  [Race_ID] INT NOT NULL,
+  [Gender_ID] INT NOT NULL,
+  [Description] varchar(500) NULL,
+  CONSTRAINT [FK_Assailant.Gender_ID]
+    FOREIGN KEY ([Gender_ID])
+      REFERENCES [Gender_Lookup]([ID]),
+  CONSTRAINT [FK_Assailant.Race_ID]
+    FOREIGN KEY ([Race_ID])
+      REFERENCES [Race_Lookup]([ID])
+);
+GO
