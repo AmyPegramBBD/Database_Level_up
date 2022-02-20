0ed9f8a34503a5b8b40edeb1cc556e02d0f22837
nikitasmal <99259115+nikitasmal@users.noreply.github.com>
2022-02-20 16:45:20 +0200

    Added the new merged functions

--- SCA_DB_script.sql
+++ SCA_DB_script.sql
@@ -22,6 +22,35 @@ CREATE TABLE dbo.[Race_Lookup] (
 )
 GO
 
+CREATE TABLE dbo.[User] (
+  [ID] INT IDENTITY (1,1) PRIMARY KEY NOT NULL,
+  [Gender_ID] INT NOT NULL,
+  [Race_ID] INT NOT NULL,
+  [Phone_Number] varchar(15) NULL,
+  [Age] INT NULL,
+  CONSTRAINT [FK_User.Gender_ID]
+    FOREIGN KEY ([Gender_ID])
+      REFERENCES [Gender_Lookup]([ID]),
+  CONSTRAINT [FK_User.Race_ID]
+    FOREIGN KEY ([Race_ID])
+      REFERENCES [Race_Lookup]([ID])
+);
+GO
+
+CREATE TABLE dbo.[Assailant] (
+  [ID] INT IDENTITY (1,1) PRIMARY KEY NOT NULL,
+  [Race_ID] INT NOT NULL,
+  [Gender_ID] INT NOT NULL,
+  [Description] varchar(500) NULL,
+  CONSTRAINT [FK_Assailant.Gender_ID]
+    FOREIGN KEY ([Gender_ID])
+      REFERENCES [Gender_Lookup]([ID]),
+  CONSTRAINT [FK_Assailant.Race_ID]
+    FOREIGN KEY ([Race_ID])
+      REFERENCES [Race_Lookup]([ID])
+);
+GO
+
 CREATE TABLE [Area] (
   [AreaID] [int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
   [ZIP_Code] [int],
@@ -56,8 +85,10 @@ CREATE TABLE dbo.[Incident] (
     FOREIGN KEY ([User_ID])
       REFERENCES [User]([ID])
 );
+GO
 
 CREATE TABLE dbo.[Incident_Type] (
   [ID] INT IDENTITY (1,1) NOT NULL PRIMARY KEY,
   [Incident_Description] varchar(150)
 );
+GO
