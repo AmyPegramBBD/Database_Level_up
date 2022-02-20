daf0c79ce87e4a84946941d53ab46cb66382caa2
RiselleRawthee <99952524+RiselleRawthee@users.noreply.github.com>
2022-02-20 15:05:34 +0200

    Gender and Race lookup tables have been added
    Let me know if this is okay

--- SCA_DB_script.sql
+++ SCA_DB_script.sql
@@ -6,3 +6,16 @@ GO
 
 USE SCA_DB;
 GO
+
+
+CREATE TABLE dbo.[Gender_Lookup] (
+  [ID] [int] IDENTITY (1,1) NOT NULL PRIMARY KEY,
+  [Gender_Description] [varchar](150) NOT NULL
+ 
+);
+
+CREATE TABLE dbo.[Race_Lookup] (
+  [ID] [int] IDENTITY (1,1) NOT NULL PRIMARY KEY,
+  [Race_Description] varchar(150) NOT NULL
+ 
+)
