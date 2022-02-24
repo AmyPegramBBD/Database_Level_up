5206330e474af15c40285c6a2863a21ece6b86a5
Amy Pegram <97732277+AmyPegramBBD@users.noreply.github.com>
2022-02-24 13:45:17 +0200

    fix: ensuring all scripts compile

--- WeekIncidents_UDF.sql
+++ WeekIncidents_UDF.sql
@@ -1,18 +1,19 @@
+USE SCA_DB
+GO
 
-
-CREATE FUNCTION getAreaRecentCrimes(@areacity varchar(120))  
+CREATE FUNCTION dbo.getAreaRecentCrimes(@areacity varchar(120))  
 RETURNS TABLE  
 AS  
 RETURN  
-    SELECT DISTINCT Area.City, Incident_Type.Incident_Description, Incident.Description, Incident.Date
+    SELECT DISTINCT Area.City, Incident_Type.Incident_Description AS [Type Description], Incident.[Incident_Description], Incident.[DateCreated]
 	FROM Area
-	INNER JOIN Location ON Location.Area_Code_ID = Area_Code_ID AND  Area.City = @areacity
-	INNER JOIN Incident ON Incident.Location_ID =Location.Location_ID
-	INNER JOIN Incident_Report ON	Incident_Report.Incident_ID =Incident.Incident_ID
-	INNER JOIN Incident_Type ON Incident_Type.Incident_Type_ID =Incident_Report.Incident_Type_ID
-	where Incident.Date < GETDATE() AND Incident.Date > DATEADD (week, -1,GETDATE()) 
-	 
+	INNER JOIN [Location] ON [Location].[Area_Code_ID] = Area_Code_ID AND  Area.City = @areacity
+	INNER JOIN [Incident] ON [Incident].Location_ID =[Location].Location_ID
+	INNER JOIN [Incident_Report] ON	Incident_Report.Incident_ID =Incident.[Incident_ID]
+	INNER JOIN [Incident_Type] ON [Incident_Type].[Incident_Type_ID] =Incident_Report.[Incident_Type_ID]
+	where Incident.[DateCreated] < GETDATE() AND Incident.[DateCreated] > DATEADD (week, -1,GETDATE()) 
 
-	 /** running the UDF example
-SELECT * from dbo.getAreaRecentCrimes('Cape Town') 
-	**/
+GO
+
+/** running the UDF example **/
+--SELECT * from dbo.getAreaRecentCrimes('Cape Town') 
