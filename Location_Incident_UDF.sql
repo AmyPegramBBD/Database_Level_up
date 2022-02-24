1dc5c6ae6ff24a2a2239b0bd84e7d116b895d271
Sandhya Singh <97591922+ms-si-Singh@users.noreply.github.com>
2022-02-24 09:53:30 +0200

    fix: Code clean up

--- Location_Incident_UDF.sql
+++ Location_Incident_UDF.sql
@@ -9,29 +9,29 @@ AS
 RETURN
 (	
     SELECT 
-	    [SCA_DB].[dbo].[Incident_Report_View].[Incident_ID],
-      [SCA_DB].[dbo].[Incident_Report_View].[Incident Description],
-      [SCA_DB].[dbo].[Incident_Report_View].[Classification],
-      [SCA_DB].[dbo].[Incident_Report_View].[Assailiant's Description]
-	FROM 
-		  [SCA_DB].[dbo].[Incident_Report_View], [Incident]
-	WHERE 
-		  [SCA_DB].[dbo].[Incident_Report_View].Incident_ID = [Incident].[Incident_ID]
-	AND
-		  [Incident].Location_ID = (SELECT DISTINCT Location_ID 
-									              FROM 
-										              [SCA_DB_V01].[dbo].[Location] L 
-									              WHERE 
-										              L.Street_Name = @streetName
-									              AND
-										              L.Street_Number = @streetNumber)
+    	[SCA_DB].[dbo].[Incident_Report_View].[Incident_ID],
+      	[SCA_DB].[dbo].[Incident_Report_View].[Incident Description],
+     	[SCA_DB].[dbo].[Incident_Report_View].[Classification],
+      	[SCA_DB].[dbo].[Incident_Report_View].[Assailiant's Description]
+    FROM 
+  	[SCA_DB].[dbo].[Incident_Report_View], [Incident]
+    WHERE 
+  	[SCA_DB].[dbo].[Incident_Report_View].Incident_ID = [Incident].[Incident_ID]
+    AND
+  	[Incident].Location_ID = (SELECT DISTINCT Location_ID 
+				  FROM 
+			      		[SCA_DB_V01].[dbo].[Location] L 
+			          WHERE 
+					L.Street_Name = @streetName
+				  AND
+				      	L.Street_Number = @streetNumber)
 		
 );
 GO
 
 /*
-*	Used the following SELECT statement to test UDF
+* Used the following SELECT statement to test UDF
 *
-*	SELECT * FROM getIncidentReportFilter_UDF(181, 'Long Street');
+* SELECT * FROM getIncidentReportFilter_UDF(181, 'Long Street');
 *
 */
