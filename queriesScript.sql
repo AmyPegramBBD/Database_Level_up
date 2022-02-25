640cdf5c47ab21b85121246e6e948bfab0ab409f
Amy Pegram <97732277+AmyPegramBBD@users.noreply.github.com>
2022-02-25 08:44:31 +0200

    Fix: Final push!!!!!!!!!!

--- queriesScript.sql
+++ queriesScript.sql
@@ -21,31 +21,12 @@ SELECT * from Incident_Report_View
 
 SELECT * FROM udf_getIncidentReportFilter(181, 'Long Street'); 
 
--- Phumi
--- WeekIncidents_UDF.sql
-
---SELECT * from dbo.getAreaRecentCrimes('Cape Town') 
-
 -- Amy
 -- usp_Get_Closest_Hospital.sql
 EXEC [uspGet_Closest_Services] 6;  
 
-SELECT 
-	[dbo].[vwHospital_Location].[Name], [dbo].[vwHospital_Location].[Phone_Number], 
-	[dbo].[vwHospital_Location].[Street_Number], [dbo].[vwHospital_Location].[Street_Name], 
-	[dbo].[vwHospital_Location].[Suburb],[dbo].[vwHospital_Location].City,
-	dbo.[vwHospital_Location].[Province], Map_Location.ToString() 
-	AS 'GPS Location' 
-	FROM 
-		[dbo].[vwHospital_Location] 
-	WHERE
-		Location_ID = 4 
-
  -- Riselle
  -- AreaRatingUDF.sql 
-
--- this shows all the suburbs to check area
-SELECT * FROM [Area]
     
 declare @InputAreaIDForOutputRange INT;
 -- change this value view different areas
